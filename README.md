103d4afa5f38f6d217cebee71d1b69309475e00d
Indrek Ruubel <indrekruubel@gmail.com>
2018-09-12 23:44:00 +0300

    readme

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Install all dependencies from maven
 
 ### Run it:
 Run it from the main method in Main.java (like in uni),
-once it's up, visit 'http://localhost:8080/'
+once it's up, visit 'http://localhost:8080/api/v1/health'
--- src/com/ruubel/controller/HealthController.java
+++ src/com/ruubel/controller/HealthController.java
@@ -3,18 +3,20 @@ package com.ruubel.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class ExampleController {
+@RequestMapping("/api/v1/health")
+public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<Map<String, Object>> status() {
         return new ResponseEntity<>(new HashMap<String, Object>(){{
-            put("status", "alive");
+            put("health", "alive");
         }}, HttpStatus.OK);
     }
 
